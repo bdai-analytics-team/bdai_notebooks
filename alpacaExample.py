e572b832148d4bdaf2fba809e64e7c7900e7fe1c
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2019-04-11 12:47:08 -0400

    created the script that can buy 10 shares of AAPL

--- alpacaExample.py
+++ alpacaExample.py
@@ -1,16 +1,15 @@
 import alpaca_trade_api as tradeapi
-api = tradeapi.REST('PKTSHUKFX9XQEP9EZ4T2',
-                    'je32eqYEJMCoL1aZ9glYleaTdwiGgnnTK1InwHOY')
-#order = api.submit_order(
-#    symbol='AAPL',
-#    shares=10,
-#    side='buy',
-#    limit_price=189.95,
-#    time_in_force='day',
-#)
 
-# Places a limit order
-api.submit_order('AAPL',10,'buy','limit','gtc',189.95)
+api = tradeapi.REST(
+    key_id='REPLACEME',
+    secret_key='REPLACEME',
+    base_url='https://paper-api.alpaca.markets'
+)
 
-# Lists all open orders
-orders = api.list_orders()
+order = api.submit_order(
+    symbol='AAPL',
+    shares=10,
+    side='buy',
+    limit_price=190,
+    time_in_force='day',
+)
